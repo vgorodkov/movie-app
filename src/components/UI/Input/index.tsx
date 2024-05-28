746af1ae29ee28f3ccc19428c9fd07254415b2fc
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-28 12:13:50 +0300

    chore: add error color

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {TextInputProps} from 'react-native';
+import {useTheme} from 'styled-components';
 
 import {Typography} from '@/components/UI/Typography';
 import {TypographyVariant} from '@/components/UI/Typography/types';
@@ -14,15 +15,20 @@ export interface InputProps extends TextInputProps {
 }
 
 export const Input = ({icon, errorMessage, ...props}: InputProps) => {
+  const theme = useTheme();
+
   return (
     <>
       <FlexContainer alignItems="center" flexFlow="row nowrap" gap={spacing.s}>
         {icon && <InputIcon source={icon} />}
-        <InputField {...props} placeholderTextColor={'white'} />
+        <InputField
+          {...props}
+          placeholderTextColor={theme.colors.primaryText}
+        />
       </FlexContainer>
       {errorMessage && (
         <Typography
-          color="coral"
+          color={theme.colors.error}
           variant={TypographyVariant.LABEL_SMALL}
           alightSelf="flex-start">
           {errorMessage}
--- src/theme/darkTheme.ts
+++ src/theme/darkTheme.ts
@@ -2,5 +2,6 @@ export const darkTheme = {
   colors: {
     background: '#1E1F27',
     primaryText: '#FFFFFF',
+    error: '#F44336',
   },
 };
