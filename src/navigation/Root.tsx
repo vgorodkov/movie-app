6b4cbaf167c6e0364aff54a1f0af01a85d949074
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-28 18:24:15 +0300

    chore: return protected routes if the user signed in

--- src/navigation/Root.tsx
+++ src/navigation/Root.tsx
@@ -1,32 +1,48 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {AuthRoutes} from '@/constants/routes';
+import {AuthRoutes, ProtectedRoutes} from '@/constants/routes';
+import {useAuth} from '@/hooks/useAuth';
 import {SignIn} from '@/screens/SignIn';
 import {SignUpScreen} from '@/screens/SignUp';
 import {WelcomeScreen} from '@/screens/Welcome';
 import {RootStackParamList} from '@/types/navigation';
 
+import {BottomTabNavigation} from './BottomTab';
+
 const StackNavigator = createNativeStackNavigator<RootStackParamList>();
 
 export const RootNavigation = () => {
+  const {initializing, user} = useAuth();
+
+  if (initializing) {
+    return null;
+  }
+
   return (
-    <StackNavigator.Navigator>
-      <StackNavigator.Group screenOptions={{headerShown: false}}>
+    <StackNavigator.Navigator screenOptions={{headerShown: false}}>
+      {user ? (
         <StackNavigator.Screen
-          name={AuthRoutes.WELCOME}
-          component={WelcomeScreen}
+          name={ProtectedRoutes.BOTTOM_TAB}
+          component={BottomTabNavigation}
         />
-        <StackNavigator.Screen
-          name={AuthRoutes.SIGN_UP}
-          component={SignUpScreen}
-          options={{presentation: 'transparentModal'}}
-        />
-        <StackNavigator.Screen
-          name={AuthRoutes.SIGN_IN}
-          component={SignIn}
-          options={{presentation: 'transparentModal'}}
-        />
-      </StackNavigator.Group>
+      ) : (
+        <>
+          <StackNavigator.Screen
+            name={AuthRoutes.WELCOME}
+            component={WelcomeScreen}
+          />
+          <StackNavigator.Screen
+            name={AuthRoutes.SIGN_UP}
+            component={SignUpScreen}
+            options={{presentation: 'transparentModal'}}
+          />
+          <StackNavigator.Screen
+            name={AuthRoutes.SIGN_IN}
+            component={SignIn}
+            options={{presentation: 'transparentModal'}}
+          />
+        </>
+      )}
     </StackNavigator.Navigator>
   );
 };
