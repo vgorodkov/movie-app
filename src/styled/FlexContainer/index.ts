ddfdb92024779549b8ccb191d01fb29bd30aaf7f
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-28 12:10:31 +0300

    chore: add fullsize container

--- src/styled/FlexContainer/index.ts
+++ src/styled/FlexContainer/index.ts
@@ -1,23 +1,32 @@
 import styled from 'styled-components/native';
 
-export const FlexContainer = styled.View<{
+interface FlexContainerProps {
   alignItems?: string;
   justifyContent?: string;
   flexFlow?: string;
   gap?: string;
-}>`
+}
+
+export const FlexContainer = styled.View<FlexContainerProps>`
   display: flex;
-  align-items: ${props => props.alignItems || 'stretch'};
-  justify-content: ${props => props.justifyContent || 'flex-start'};
-  flex-flow: ${props => props.flexFlow || 'column nowrap'};
-  gap: ${props => props.gap || 0};
+  align-items: ${({alignItems = 'stretch'}) => alignItems};
+  justify-content: ${({justifyContent = 'flex-start'}) => justifyContent};
+  flex-flow: ${({flexFlow = 'column nowrap'}) => flexFlow};
+  gap: ${({gap = '0'}) => gap};
 `;
 
-export const FullscreenContainer = styled(FlexContainer)<{
+interface FullscreenContainerProps extends FlexContainerProps {
   paddingHorizontal?: string;
   paddingVertical?: string;
-}>`
+}
+
+export const FullscreenContainer = styled(
+  FlexContainer,
+)<FullscreenContainerProps>`
   flex: 1;
-  padding: ${props => props.paddingVertical} ${props => props.paddingHorizontal};
-  background-color: ${props => props.theme.colors.background};
+  padding-top: ${({paddingVertical = '0'}) => paddingVertical};
+  padding-bottom: ${({paddingVertical = '0'}) => paddingVertical};
+  padding-left: ${({paddingHorizontal = '0'}) => paddingHorizontal};
+  padding-right: ${({paddingHorizontal = '0'}) => paddingHorizontal};
+  background-color: ${({theme}) => theme.colors.background};
 `;
