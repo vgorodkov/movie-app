8590f41b00f56932201b559d5605c95fb0a483bf
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-28 12:09:01 +0300

    chore: add navigation types

---
+++ src/types/navigation.ts
@@ -0,0 +1,7 @@
+import {AuthRoutes} from '@/constants/routes';
+
+export type RootStackParamList = {
+  [AuthRoutes.WELCOME]: undefined;
+  [AuthRoutes.SIGN_UP]: undefined;
+  [AuthRoutes.SIGN_IN]: undefined;
+};
