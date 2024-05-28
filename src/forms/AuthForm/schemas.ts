147894b601604a052b0a6ef8df9b7b27404b329f
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-28 18:22:33 +0300

    chore: add email validation with regexp

--- src/forms/AuthForm/schemas.ts
+++ src/forms/AuthForm/schemas.ts
@@ -1,5 +1,7 @@
 import * as yup from 'yup';
 
+const emailRegexp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
 export const signUpValidationSchema = yup.object().shape({
   name: yup
     .string()
@@ -11,10 +13,14 @@ export const signUpValidationSchema = yup.object().shape({
     .required('Surname is required')
     .min(2, 'Surname must be at least 2 characters')
     .max(16, 'Surname must be max 16 characters'),
-  email: yup.string().email('Invalid email').required('Email is required'),
+  email: yup
+    .string()
+    .email('Invalid email')
+    .required('Email is required')
+    .matches(emailRegexp, 'Invalid email'),
   password: yup
     .string()
-    .min(4, 'Password must be at least 4 characters')
+    .min(6, 'Password must be at least 6 characters')
     .max(16, 'Password must be max 16 characters')
     .required('Password is required'),
 });
