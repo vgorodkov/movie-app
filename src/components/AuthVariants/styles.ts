f90f4227bf8e55fa957966d12a07905575bcd61c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-28 12:03:38 +0300

    refactor: remove redundant style

--- src/components/AuthVariants/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components/native';
-
-import {Typography} from '@/styled/Typography';
-
-export const UnderlinedTypography = styled(Typography)`
-  text-decoration: underline;
-`;
