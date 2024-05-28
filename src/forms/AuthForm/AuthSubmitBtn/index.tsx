df0a4cf7ce4a03d9f8298f423b9316a888cc1b4c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-28 18:22:51 +0300

    feat: add signup and signin via email/password

--- src/forms/AuthForm/AuthSubmitBtn/index.tsx
+++ src/forms/AuthForm/AuthSubmitBtn/index.tsx
@@ -1,3 +1,4 @@
+import auth from '@react-native-firebase/auth';
 import React from 'react';
 import {FieldValues, useFormContext} from 'react-hook-form';
 
@@ -6,7 +7,38 @@ import {AuthButton} from './styles';
 export const AuthSubmitButton = ({isSignUp}: {isSignUp: boolean}) => {
   const {handleSubmit} = useFormContext<FieldValues>();
 
+  const handleSignUp = (data: FieldValues) => {
+    const {email, password} = data;
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+
+  const handleSignIn = (data: FieldValues) => {
+    const {email, password} = data;
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   const onSumbit = (data: FieldValues) => {
+    if (isSignUp) {
+      handleSignUp(data);
+    } else {
+      handleSignIn(data);
+    }
     console.log(data);
   };
 
