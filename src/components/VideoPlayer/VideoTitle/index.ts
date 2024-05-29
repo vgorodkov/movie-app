6db5a2d4c3edaaa45a8152fe0638fbe91b1638ed
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-29 21:33:39 +0300

    feat: create VideoTitle

---
+++ src/components/VideoPlayer/VideoTitle/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+import {Typography} from '@/components/UI/Typography';
+import {spacing} from '@/constants/spacing';
+
+export const VideoTitle = styled(Typography)`
+  position: absolute;
+  top: ${spacing.sm};
+  left: ${spacing.sm};
+`;
