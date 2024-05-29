b1d2d422fdde40d9a8209c5a85629f36b4e54a06
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-29 16:10:53 +0300

    feat: create HomeScreen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,58 @@
-import auth from '@react-native-firebase/auth';
-
-import {Button} from '@/components/UI/Button';
+import {MovieCarousel} from '@/components/MovieCarousel';
+import {Typography} from '@/components/UI/Typography';
+import {TypographyVariant} from '@/components/UI/Typography/types';
+import {VideoPlayer} from '@/components/VideoPlayer';
 import {FullscreenContainer} from '@/styled/FlexContainer';
 
+import {ComingSoonContainer, MovieCarouselContainer} from './styles';
+
+const MOCK_DATA = [
+  {
+    id: 1,
+    title: 'Spider-Man:No Way Home',
+    tags: ['13+', 'Action', 'IMAX'],
+    img: require('@/assets/imgs/movie_2.png'),
+  },
+  {
+    id: 2,
+    title: 'Spider-Man:No Way Home',
+    tags: ['13+', 'Action', 'IMAX'],
+    img: require('@/assets/imgs/movie_2.png'),
+  },
+  {
+    id: 3,
+    title: 'Spider-Man:No Way Home',
+    tags: ['13+', 'Action', 'IMAX'],
+    img: require('@/assets/imgs/movie_3.png'),
+  },
+  {
+    id: 4,
+    title: 'Spider-Man:No Way Home',
+    tags: ['13+', 'Action', 'IMAX'],
+    img: require('@/assets/imgs/movie_3.png'),
+  },
+  {
+    id: 5,
+    title: 'Spider-Man:No Way Home',
+    tags: ['13+', 'Action', 'IMAX'],
+    img: require('@/assets/imgs/movie_3.png'),
+  },
+];
+
 export const HomeScreen = () => {
-  const onLogOutBtnPress = () => {
-    auth().signOut();
-  };
   return (
-    <FullscreenContainer>
-      <Button onPress={onLogOutBtnPress}>Log out</Button>
+    <FullscreenContainer gap="16px">
+      <ComingSoonContainer>
+        <Typography
+          alightSelf="flex-start"
+          variant={TypographyVariant.SUBTITLE_LARGE}>
+          Coming Soon
+        </Typography>
+        <VideoPlayer />
+      </ComingSoonContainer>
+      <MovieCarouselContainer>
+        <MovieCarousel data={MOCK_DATA} title="Now Showing" />
+      </MovieCarouselContainer>
     </FullscreenContainer>
   );
 };
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const ComingSoonContainer = styled.View`
+  flex: 0.35;
+  padding: 0 32px;
+  gap: 8px;
+`;
+
+export const MovieCarouselContainer = styled.View`
+  flex: 0.5;
+`;
