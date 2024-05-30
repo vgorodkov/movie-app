589e5efb1cbe5dd9be4a552cd2b56629ca284f21
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-30 20:43:12 +0300

    refactor: use exsiting MovieGenreRow component

--- src/components/MovieDetails/BasicDetails/index.tsx
+++ src/components/MovieDetails/BasicDetails/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import {MovieTag} from '@/components/MovieCarousel/MovieItem/styles';
-import {Typography, TypographyVariant} from '@/components/UI';
-import {Icon} from '@/components/UI/Icon';
+import {MovieGenreRow} from '@/components/MovieGenreRow';
+import {Icon, Typography, TypographyVariant} from '@/components/UI';
 import {spacing} from '@/constants/spacing';
 import {FlexContainer} from '@/styled/FlexContainer';
 
@@ -11,15 +10,7 @@ import {BasicDetailsProps} from './types';
 export const BasicDetails = ({genre}: BasicDetailsProps) => {
   return (
     <FlexContainer justifyContent="space-between" flexFlow="row nowrap">
-      <FlexContainer flexFlow="row wrap" gap={spacing.sm}>
-        {genre.map(item => (
-          <MovieTag key={item}>
-            <Typography variant={TypographyVariant.LABEL_MEDIUM}>
-              {item}
-            </Typography>
-          </MovieTag>
-        ))}
-      </FlexContainer>
+      <MovieGenreRow genre={genre} />
       <FlexContainer alignItems="center" gap={spacing.xs} flexFlow="row nowrap">
         <Icon
           width={16}
