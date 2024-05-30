4433756b4f554a5cbf97942023a690cbe1a67fc1
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-30 19:53:51 +0300

    refactor: decompose screen options

--- src/navigation/Root/options.ts
+++ src/navigation/Root/options.ts
@@ -1,3 +1,17 @@
+import {NativeStackNavigationOptions} from '@react-navigation/native-stack';
+
 export const stackNavigatorOptions = {
   headerShown: false,
+};
+
+export const transparentModalOptions: NativeStackNavigationOptions = {
+  presentation: 'transparentModal',
+  animation: 'fade',
+};
+
+export const movieDetailsScreenOptions: NativeStackNavigationOptions = {
+  headerShown: true,
+  headerTransparent: true,
+  headerTitle: '',
+  headerTintColor: 'white',
 };
