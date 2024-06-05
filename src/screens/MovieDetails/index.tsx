c33cb0776d5a84b63d865438c0b182d5f663bb8d
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-05 20:11:50 +0300

    chore: add route props to BookingTicket screen

--- src/screens/MovieDetails/index.tsx
+++ src/screens/MovieDetails/index.tsx
@@ -8,10 +8,10 @@ import {FullscreenContainer} from '@/styled/FlexContainer';
 import {MovieDetailsScreenProps} from './types';
 
 export const MovieDetailsScreen = ({route}: MovieDetailsScreenProps) => {
-  const {title, synopsis, genre, imdbid} = route.params;
+  const {title, synopsis, genre, imdbid, imageurl} = route.params;
   return (
     <FullscreenContainer>
-      <MovieDetailsPoster />
+      <MovieDetailsPoster title={title} imageurl={imageurl} />
       <MovieDetails genre={genre} title={title} synopsis={synopsis} />
       <MovieComments imdbid={imdbid} />
     </FullscreenContainer>
