0be913d6ec76b702fbee87ab09f503cff1521056
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-05 20:06:08 +0300

    feat: create BookingSubmit

---
+++ src/components/TicketBooking/BookingSubmit/index.tsx
@@ -0,0 +1,83 @@
+import {useNavigation} from '@react-navigation/native';
+import {nanoid} from '@reduxjs/toolkit';
+import React, {useState} from 'react';
+
+import {Typography, TypographyVariant} from '@/components/UI';
+import {SEAT_PRICE} from '@/constants/cinema';
+import {ProtectedRoutes} from '@/constants/routes';
+import {useAppSelector} from '@/store/hooks';
+import {ticketBookingSelector} from '@/store/slices/ticketBooking/selectors';
+import {FlexContainer} from '@/styled/FlexContainer';
+import {BookingTicket} from '@/types/movies';
+import {getEncryptedData, saveEncryptedData} from '@/utils/encryptedStorage';
+
+import {BookingSubmitButton} from './styles';
+
+export const BookingSubmit = ({
+  imageUrl,
+  title,
+}: {
+  imageUrl: string;
+  title: string;
+}) => {
+  const navigation = useNavigation();
+  const ticketBookingInfo = useAppSelector(ticketBookingSelector);
+  const [error, setError] = useState<string | null>(null);
+  const {selectedSeats, selectedDate} = ticketBookingInfo;
+  const selectedSeatsAmount = selectedSeats.length;
+  const isButtonActive = selectedDate && selectedSeatsAmount > 0;
+
+  const onBookingSubmitBtnPress = async () => {
+    const newBooking = {
+      ticketId: nanoid(),
+      movieName: title,
+      movieImgUrl: imageUrl,
+      date: ticketBookingInfo.selectedDate!,
+      price: ticketBookingInfo.selectedSeats.length * SEAT_PRICE,
+      seatsAmount: ticketBookingInfo.selectedSeats.length,
+    };
+
+    try {
+      const existingData = await getEncryptedData<BookingTicket[]>('booking');
+      if (existingData) {
+        const updatedData = [...existingData, newBooking];
+        await saveEncryptedData('booking', updatedData);
+      } else {
+        await saveEncryptedData('booking', [newBooking]);
+      }
+      navigation.navigate(ProtectedRoutes.BOTTOM_TAB);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      }
+    }
+  };
+
+  return (
+    <>
+      {error && (
+        <Typography
+          variant={TypographyVariant.LABEL_LARGE}
+          color="red"
+          alightSelf="flex-end">
+          {error}
+        </Typography>
+      )}
+      <FlexContainer flexFlow="row nowrap" justifyContent="space-between">
+        <FlexContainer>
+          <Typography variant={TypographyVariant.LABEL_MEDIUM}>
+            {selectedSeatsAmount} Seats
+          </Typography>
+          <Typography variant={TypographyVariant.SUBTITLE_LARGE}>
+            {SEAT_PRICE * selectedSeatsAmount} $
+          </Typography>
+        </FlexContainer>
+        <BookingSubmitButton
+          onPress={onBookingSubmitBtnPress}
+          disabled={!isButtonActive}>
+          Book Now
+        </BookingSubmitButton>
+      </FlexContainer>
+    </>
+  );
+};
---
+++ src/components/TicketBooking/BookingSubmit/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+
+import {Button} from '@/components/UI';
+
+export const BookingSubmitButton = styled(Button)`
+  width: 60%;
+`;
