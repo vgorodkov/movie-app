895ae990bbd3f0dcea5e8d9130ad0a293bd332cb
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-05 20:04:33 +0300

    feat: create BookingDatepicker

---
+++ src/components/TicketBooking/BookingDatepicker/index.tsx
@@ -0,0 +1,46 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {useTheme} from 'styled-components';
+
+import {PressableIcon, Typography, TypographyVariant} from '@/components/UI';
+import {ProtectedRoutes} from '@/constants/routes';
+import {useAppSelector} from '@/store/hooks';
+import {bookingDateSelector} from '@/store/slices/ticketBooking/selectors';
+import {FlexContainer} from '@/styled/FlexContainer';
+
+export const BookingDatepicker = () => {
+  const theme = useTheme();
+  const navigation = useNavigation();
+  const selectedDate = useAppSelector(bookingDateSelector);
+
+  const onDatepickerIconPress = () => {
+    navigation.navigate(ProtectedRoutes.BOOKING_DATEPICKER);
+  };
+
+  return (
+    <FlexContainer
+      flexFlow="row nowrap"
+      justifyContent="space-between"
+      alignItems="flex-end">
+      <FlexContainer>
+        <Typography
+          alightSelf="flex-start"
+          variant={TypographyVariant.LABEL_LARGE}>
+          Schedule
+        </Typography>
+        <Typography
+          color={selectedDate ? theme.colors.primaryText : theme.colors.error}
+          alightSelf="flex-start"
+          variant={TypographyVariant.BODY_MEDIUM}>
+          Date: {selectedDate ?? 'No date selected'}
+        </Typography>
+      </FlexContainer>
+      <PressableIcon
+        onPress={onDatepickerIconPress}
+        icon={require('@/assets/icons/calendar.png')}
+        width={24}
+        height={24}
+      />
+    </FlexContainer>
+  );
+};
