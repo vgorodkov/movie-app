b988ab8f5d0899461f0d75c56a5453d459b426ca
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-05 20:17:26 +0300

    chore: add Toast notification if the error occurrs

--- src/components/AuthVariants/index.tsx
+++ src/components/AuthVariants/index.tsx
@@ -1,4 +1,5 @@
 import {useNavigation} from '@react-navigation/native';
+import {useState} from 'react';
 
 import {Button} from '@/components/UI';
 import {AuthRoutes} from '@/constants/routes';
@@ -7,11 +8,14 @@ import {FlexContainer} from '@/styled/FlexContainer';
 import {facebookSignIn} from '@/utils/facebookSignIn';
 import {googleSignIn} from '@/utils/googleSignIn';
 
+import {Toast} from '../UI/Toast';
 import {AUTH_VARIANTS} from './constants';
 import {LogInLink} from './LogInLink';
 
 export const AuthVariants = () => {
   const navigation = useNavigation();
+
+  const [error, setError] = useState<null | string>(null);
 
   const onAccountCreateBtnPress = () => {
     navigation.navigate(AuthRoutes.SIGN_UP);
@@ -26,22 +30,41 @@ export const AuthVariants = () => {
     github: onGithubAuthBtnPress,
   };
 
+  const onAuthVariantPress =
+    (name: 'account' | 'google' | 'facebook' | 'github') => async () => {
+      try {
+        await authFunctions[name]();
+      } catch (err) {
+        if (err.code === 'auth/account-exists-with-different-credential') {
+          setError(
+            ' An account already exists with the same email address but different sign-in credentials',
+          );
+        } else {
+          setError('Error with auth');
+        }
+        setTimeout(() => setError(null), 3000);
+      }
+    };
+
   return (
-    <FlexContainer gap={spacing.m}>
-      {AUTH_VARIANTS.map(button => {
-        const {icon, backgroundColor, color, label, name} = button;
-        return (
-          <Button
-            onPress={authFunctions[name]}
-            key={label}
-            icon={icon}
-            backgroundColor={backgroundColor}
-            color={color}>
-            {label}
-          </Button>
-        );
-      })}
-      <LogInLink />
-    </FlexContainer>
+    <>
+      <FlexContainer gap={spacing.m}>
+        {AUTH_VARIANTS.map(button => {
+          const {icon, backgroundColor, color, label, name} = button;
+          return (
+            <Button
+              onPress={onAuthVariantPress(name)}
+              key={label}
+              icon={icon}
+              backgroundColor={backgroundColor}
+              color={color}>
+              {label}
+            </Button>
+          );
+        })}
+        <LogInLink />
+      </FlexContainer>
+      {error && <Toast content={error} />}
+    </>
   );
 };
