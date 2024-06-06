abe4edeff4837c72df8b05e3eaf2df6070018fd2
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-06 12:53:10 +0300

    refactor: improve code readability; decomposition

--- src/forms/AuthForm/AuthSubmitBtn/index.tsx
+++ src/forms/AuthForm/AuthSubmitBtn/index.tsx
@@ -1,69 +1,44 @@
 import auth from '@react-native-firebase/auth';
 import React, {useState} from 'react';
 import {FieldValues, useFormContext} from 'react-hook-form';
-import {useTheme} from 'styled-components';
 
-import {Typography, TypographyVariant} from '@/components/UI';
-import {spacing} from '@/constants/spacing';
-import {FlexContainer} from '@/styled/FlexContainer';
+import {LoadingBackdrop} from '@/components/UI/LoadingBackdrop';
+import {handleAuthError} from '@/utils/handleAuthError';
 
 import {AuthButton} from './styles';
+import {AuthSubmitButtonProps} from './types';
 
-export const AuthSubmitButton = ({isSignUp}: {isSignUp: boolean}) => {
-  const theme = useTheme();
+export const AuthSubmitButton = ({isSignUp}: AuthSubmitButtonProps) => {
   const {handleSubmit} = useFormContext<FieldValues>();
-  const [authError, setAuthError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSignUp = (data: FieldValues) => {
+  const handleAuth = async (data: FieldValues) => {
     const {email, password} = data;
-    auth()
-      .createUserWithEmailAndPassword(email, password)
-      .catch(error => {
-        if (error.code === 'auth/email-already-in-use') {
-          setAuthError('That email address is already in use!');
-        }
+    setIsLoading(true);
 
-        if (error.code === 'auth/invalid-email') {
-          setAuthError('That email address is invalid!');
-        }
-
-        console.error(error);
-      });
-  };
-
-  const handleSignIn = (data: FieldValues) => {
-    const {email, password} = data;
-    auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch(error => {
-        if (error.code === 'auth/invalid-credential') {
-          setAuthError('Auth credential is invalid!');
-        }
-        console.error(error);
-      });
-  };
-
-  const onSubmit = (data: FieldValues) => {
-    if (isSignUp) {
-      handleSignUp(data);
-    } else {
-      handleSignIn(data);
+    try {
+      if (isSignUp) {
+        await auth().createUserWithEmailAndPassword(email, password);
+      } else {
+        await auth().signInWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      handleAuthError(error as {code: string; message: string});
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  const onSubmit = (data: FieldValues) => {
+    handleAuth(data);
+  };
+
   return (
-    <FlexContainer gap={spacing.s}>
-      {authError && (
-        <Typography
-          color={theme.colors.error}
-          variant={TypographyVariant.LABEL_MEDIUM}
-          alightSelf="flex-start">
-          {authError}
-        </Typography>
-      )}
-      <AuthButton onPress={handleSubmit(onSubmit)}>
+    <>
+      {isLoading && <LoadingBackdrop />}
+      <AuthButton disabled={isLoading} onPress={handleSubmit(onSubmit)}>
         {isSignUp ? 'Sign Up' : 'Sign In'}
       </AuthButton>
-    </FlexContainer>
+    </>
   );
 };
---
+++ src/forms/AuthForm/AuthSubmitBtn/types.ts
@@ -0,0 +1,3 @@
+export interface AuthSubmitButtonProps {
+  isSignUp: boolean;
+}
