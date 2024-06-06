332e30636ea43658be8f116c8d9642b6d84d626f
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-06 11:44:37 +0300

    chore: remove api key

--- src/store/apiSlices/movieApi.ts
+++ src/store/apiSlices/movieApi.ts
@@ -1,7 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 import {MovieApiResponse} from '@/types/movies';
-
 const baseUrl = 'https://ott-details.p.rapidapi.com';
 
 export const movieApi = createApi({
@@ -9,14 +8,10 @@ export const movieApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: baseUrl,
     prepareHeaders: headers => {
-      headers.set(
-        'X-RapidAPI-Key',
-        '3ba07edcd0msh50939710fa56e37p1dc684jsn276da19bd4bb',
-      );
+      headers.set('X-RapidAPI-Key', process.env.RAPID_API_KEY as string);
       return headers;
     },
   }),
-  keepUnusedDataFor: 6000000000,
   endpoints: builder => ({
     getMovies: builder.query<MovieApiResponse, string>({
       query: genre =>
