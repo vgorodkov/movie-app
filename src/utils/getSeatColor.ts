be5b8604794d917b8ed441deba1d5e0877af7130
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-11 16:27:53 +0300

    refactor: remove redundant util

--- src/utils/getSeatColor.ts
+++
@@ -1,9 +0,0 @@
-export const getSeatColor = (isReserved: boolean, isSelected: boolean) => {
-  if (isReserved) {
-    return '#C4C4C4';
-  }
-  if (isSelected) {
-    return '#D98639';
-  }
-  return 'transparent';
-};
