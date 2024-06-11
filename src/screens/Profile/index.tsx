a0548597ac27f318085de8068a3cc3b25d8df48d
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-11 16:28:17 +0300

    feat: add toggle theme btn to profile screen

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,16 +1,31 @@
 import auth from '@react-native-firebase/auth';
 import React from 'react';
 
+import {ToggleThemeBtn} from '@/components/ToggleThemeBtn';
 import {Button} from '@/components/UI';
-import {FullscreenContainer} from '@/styled/FlexContainer';
+import {spacing} from '@/constants/spacing';
+import {FlexContainer, FullscreenContainer} from '@/styled/FlexContainer';
 
 export const ProfileScreen = () => {
   const onLogOutBtnPress = () => {
     auth().signOut();
   };
+
+  const onEditBtnPress = () => {};
+
+  const onSettingsBtnPress = () => {};
+
+  const onPrivatePolicyBtnPress = () => {};
+
   return (
-    <FullscreenContainer>
-      <Button onPress={onLogOutBtnPress}>Log Out</Button>
+    <FullscreenContainer gap={spacing.l} alignItems="center">
+      <FlexContainer gap={spacing.m}>
+        <Button onPress={onEditBtnPress}>Edit profile info</Button>
+        <Button onPress={onSettingsBtnPress}>Settings</Button>
+        <Button onPress={onPrivatePolicyBtnPress}>Private Policy</Button>
+        <Button onPress={onLogOutBtnPress}>Log Out</Button>
+      </FlexContainer>
+      <ToggleThemeBtn />
     </FullscreenContainer>
   );
 };
