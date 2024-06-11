311b116bb21e37266d2d705399cac1686c63c296
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-11 16:27:34 +0300

    chore: use Svgr with theme color instead of imgs

---
+++ src/navigation/BottomTab/getIcon.tsx
@@ -0,0 +1,56 @@
+import {DefaultTheme} from 'styled-components/native';
+
+import Home from '@/assets/icons/home.svg';
+import Profile from '@/assets/icons/profile.svg';
+import Ticket from '@/assets/icons/ticket.svg';
+import Top from '@/assets/icons/top.svg';
+import {BottomTabRoutes} from '@/constants/routes';
+
+export const getRouteIcon = (
+  route: string,
+  focused: boolean,
+  theme: DefaultTheme,
+) => {
+  switch (route) {
+    case BottomTabRoutes.HOME:
+      return (
+        <Home
+          style={{
+            color: focused
+              ? theme.colors.primaryText
+              : theme.colors.disabledText,
+          }}
+        />
+      );
+    case BottomTabRoutes.TOP_MOVIES:
+      return (
+        <Top
+          style={{
+            color: focused
+              ? theme.colors.primaryText
+              : theme.colors.disabledText,
+          }}
+        />
+      );
+    case BottomTabRoutes.TICKETS:
+      return (
+        <Ticket
+          style={{
+            color: focused
+              ? theme.colors.primaryText
+              : theme.colors.disabledText,
+          }}
+        />
+      );
+    case BottomTabRoutes.PROFILE:
+      return (
+        <Profile
+          style={{
+            color: focused
+              ? theme.colors.primaryText
+              : theme.colors.disabledText,
+          }}
+        />
+      );
+  }
+};
--- src/navigation/BottomTab/options.tsx
+++ src/navigation/BottomTab/options.tsx
@@ -1,22 +1,17 @@
 import {BottomTabNavigationOptions} from '@react-navigation/bottom-tabs';
-import {Image} from 'react-native';
+import {DefaultTheme} from 'styled-components/native';
 
 import {getRouteIcon} from './getIcon';
 import {styles} from './styles';
 import {BottomTabBarOptionsProps} from './types';
 
-export const bottomTabBarOptions = ({
-  route,
-}: BottomTabBarOptionsProps): BottomTabNavigationOptions => {
-  return {
-    headerShown: false,
-    tabBarStyle: styles.tabBarStyle,
-    tabBarShowLabel: false,
-    tabBarIcon: ({focused}) => (
-      <Image
-        style={styles.tabBarIcon}
-        source={getRouteIcon(route.name, focused)}
-      />
-    ),
+export const bottomTabBarOptions =
+  (theme: DefaultTheme) =>
+  ({route}: BottomTabBarOptionsProps): BottomTabNavigationOptions => {
+    return {
+      headerShown: false,
+      tabBarStyle: styles.tabBarStyle,
+      tabBarShowLabel: false,
+      tabBarIcon: ({focused}) => getRouteIcon(route.name, focused, theme),
+    };
   };
-};
