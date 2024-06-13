293a502a3c549fe7a91e5c9a5ab6536f7705e898
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-13 14:16:29 +0300

    refactor: rename to Dropdown

--- src/components/UI/SelectDropdown/SelectDropdownOption/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native';
-
-export const OptionTouchable = styled.TouchableOpacity<{isActive: boolean}>`
-  background-color: ${props =>
-    props.isActive ? props.theme.colors.primary : props.theme.colors.onPrimary};
-`;
--- src/components/UI/SelectDropdown/SelectDropdownOption/types.ts
+++
@@ -1,7 +0,0 @@
-import {DropdownOption} from '../types';
-
-export interface SelectDropdownOptionProps {
-  onSelect: (option: DropdownOption) => void;
-  option: DropdownOption;
-  isActive: boolean;
-}
