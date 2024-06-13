fda450c9f33665053ef8a59515ab44a05b0e4003
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-13 14:30:34 +0300

    feat: add user information to rtk user slice on signin/signup

--- src/forms/AuthForm/AuthSubmitBtn/index.tsx
+++ src/forms/AuthForm/AuthSubmitBtn/index.tsx
@@ -1,10 +1,13 @@
 import auth from '@react-native-firebase/auth';
 import React, {useState} from 'react';
 import {FieldValues, useFormContext} from 'react-hook-form';
+import {useDispatch} from 'react-redux';
 import {useTheme} from 'styled-components';
 
 import {LoadingBackdrop} from '@/components/UI/LoadingBackdrop';
+import {setUser} from '@/store/slices/user';
 import {addUserToFirestore} from '@/utils/addUserToFirestore';
+import {getUserData} from '@/utils/firebase/getUserData';
 import {handleAuthError} from '@/utils/handleAuthError';
 
 import {AuthButton} from './styles';
@@ -12,25 +15,28 @@ import {AuthSubmitButtonProps} from './types';
 
 export const AuthSubmitButton = ({isSignUp}: AuthSubmitButtonProps) => {
   const theme = useTheme();
+  const dispatch = useDispatch();
   const {handleSubmit} = useFormContext<FieldValues>();
   const [isLoading, setIsLoading] = useState(false);
 
   const handleAuth = async (data: FieldValues) => {
-    const {email, password} = data;
-    setIsLoading(true);
+    const {email, name, surname, password} = data;
 
+    setIsLoading(true);
     try {
       if (isSignUp) {
-        await auth()
-          .createUserWithEmailAndPassword(email, password)
-          .then(({user}) => {
-            addUserToFirestore(user.uid, {
-              name: data.name,
-              surname: data.surname,
-            });
-          });
+        const {user} = await auth().createUserWithEmailAndPassword(
+          email,
+          password,
+        );
+        addUserToFirestore(user.uid, {name, surname});
+        dispatch(setUser({name, surname}));
       } else {
-        await auth().signInWithEmailAndPassword(email, password);
+        const {user} = await auth().signInWithEmailAndPassword(email, password);
+        const userData = await getUserData(user.uid);
+        if (userData) {
+          dispatch(setUser({name: userData.name, surname: userData.surname}));
+        }
       }
     } catch (error) {
       handleAuthError(error as {code: string; message: string});
