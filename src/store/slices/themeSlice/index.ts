015c0b176998140cf16444dbeeeba184ce2783dd
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-13 14:21:59 +0300

    chore: create theme consts

--- src/store/slices/themeSlice/index.ts
+++ src/store/slices/themeSlice/index.ts
@@ -1,19 +1,21 @@
 import type {PayloadAction} from '@reduxjs/toolkit';
 import {createSlice} from '@reduxjs/toolkit';
 
+import {ThemeVariant} from '@/constants/theme';
+
 export interface ThemeState {
-  selectedTheme: 'dark' | 'light';
+  selectedTheme: ThemeVariant;
 }
 
 const initialState: ThemeState = {
-  selectedTheme: 'dark',
+  selectedTheme: ThemeVariant.DARK,
 };
 
 export const themeSlice = createSlice({
   name: 'theme',
   initialState,
   reducers: {
-    selectTheme: (state, action: PayloadAction<'dark' | 'light'>) => {
+    selectTheme: (state, action: PayloadAction<ThemeVariant>) => {
       state.selectedTheme = action.payload;
     },
   },
