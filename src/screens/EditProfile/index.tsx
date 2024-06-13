a6ae7ecac8135efc0081752c0cf3527392206d19
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-13 14:20:41 +0300

    feat: add EditProfileModal

---
+++ src/screens/EditProfile/index.tsx
@@ -0,0 +1,41 @@
+import {yupResolver} from '@hookform/resolvers/yup';
+import React from 'react';
+import {FormProvider, useForm} from 'react-hook-form';
+
+import {Modal} from '@/components/UI';
+import {spacing} from '@/constants/spacing';
+import {EditProfileForm} from '@/forms/EditProfileForm';
+import {editForms} from '@/forms/EditProfileForm/constants';
+import {EditFormSubmitBtn} from '@/forms/EditProfileForm/EditFormSubmitBtn';
+import {editProfileValidationSchema} from '@/forms/EditProfileForm/schema';
+import {EditProfileFormValues} from '@/forms/EditProfileForm/types';
+import {FlexContainer} from '@/styled/FlexContainer';
+
+import {EditProfileModalProps} from './types';
+
+export const EditProfileModal = ({navigation}: EditProfileModalProps) => {
+  const methods = useForm<EditProfileFormValues>({
+    resolver: yupResolver(editProfileValidationSchema),
+  });
+
+  const onBackdropPress = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <Modal title="Edit your profile" onBackdropPress={onBackdropPress}>
+      <FormProvider {...methods}>
+        <FlexContainer gap={spacing.m}>
+          {editForms.map(editForm => (
+            <EditProfileForm
+              key={editForm.title}
+              inputs={editForm.fields}
+              title={editForm.title}
+            />
+          ))}
+          <EditFormSubmitBtn />
+        </FlexContainer>
+      </FormProvider>
+    </Modal>
+  );
+};
