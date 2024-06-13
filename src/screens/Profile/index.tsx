1a81e0d576fc8386507763f31e06459a76935317
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-13 14:20:57 +0300

    refactor: decompose Profile screen

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,48 +1,25 @@
-import auth from '@react-native-firebase/auth';
-import i18next from 'i18next';
 import React from 'react';
-import {useTranslation} from 'react-i18next';
 
+import {ProfileActions} from '@/components/ProfileActions';
+import {ProfileImage} from '@/components/ProfileImage';
+import {ProfileInformation} from '@/components/ProfileInformation';
 import {ToggleThemeBtn} from '@/components/ToggleThemeBtn';
-import {Button, SelectDropdown} from '@/components/UI';
-import {languages} from '@/constants/languages';
 import {spacing} from '@/constants/spacing';
 import {FlexContainer, FullscreenContainer} from '@/styled/FlexContainer';
 
 export const ProfileScreen = () => {
-  const {t} = useTranslation('profile');
-
-  const onLogOutBtnPress = () => {
-    auth().signOut();
-  };
-
-  const onEditBtnPress = () => {};
-
-  const onSettingsBtnPress = () => {};
-
-  const onPrivatePolicyBtnPress = () => {};
-
-  const onDropdownSelect = (optionCode: string) => {
-    i18next.changeLanguage(optionCode);
-  };
-
   return (
-    <FullscreenContainer gap={spacing.l}>
-      <FlexContainer gap={spacing.m}>
-        <Button onPress={onEditBtnPress}>{t('EditBtn')}</Button>
-        <Button onPress={onSettingsBtnPress}>{t('SettingsBtn')}</Button>
-        <Button onPress={onPrivatePolicyBtnPress}>
-          {t('PrivatePolicyBtn')}
-        </Button>
-        <Button onPress={onLogOutBtnPress}>{t('LogOutBtn')}</Button>
+    <FullscreenContainer
+      gap={spacing.l}
+      justifyContent="space-between"
+      alignItems="center"
+      padding={spacing.l}>
+      <FlexContainer alignItems="center" gap={spacing.s}>
+        <ProfileImage />
+        <ProfileInformation />
       </FlexContainer>
+      <ProfileActions />
       <ToggleThemeBtn />
-      <SelectDropdown
-        initialOption={languages[0]}
-        label={t('Select language')}
-        data={languages}
-        onSelect={onDropdownSelect}
-      />
     </FullscreenContainer>
   );
 };
