ba4dca28a8b609e2812c535c5b6ebab6085cad4b
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-14 12:02:31 +0300

    feat: get data from bookedTickets slice

--- src/screens/PastTickets/index.tsx
+++ src/screens/PastTickets/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import {BookedTicketsList} from '@/components/BookedTicketsList';
+import {useAppSelector} from '@/store/hooks';
+import {pastBookedTicketsSelector} from '@/store/slices/bookedTickets/selectors';
 import {FullscreenContainer} from '@/styled/FlexContainer';
 
 export const PastTicketsScreen = () => {
+  const pastBookedTickets = useAppSelector(pastBookedTicketsSelector);
   return (
     <FullscreenContainer>
-      <BookedTicketsList ticketsFilterMode="past" />
+      <BookedTicketsList data={pastBookedTickets} />
     </FullscreenContainer>
   );
 };
--- src/screens/UpcomingTickets/index.tsx
+++ src/screens/UpcomingTickets/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import {BookedTicketsList} from '@/components/BookedTicketsList';
+import {useAppSelector} from '@/store/hooks';
+import {upcomingBookedTicketsSelector} from '@/store/slices/bookedTickets/selectors';
 import {FullscreenContainer} from '@/styled/FlexContainer';
 
 export const UpcomingTicketsScreen = () => {
+  const upcomingBookedTickets = useAppSelector(upcomingBookedTicketsSelector);
+
   return (
     <FullscreenContainer>
-      <BookedTicketsList ticketsFilterMode="upcoming" />
+      <BookedTicketsList data={upcomingBookedTickets} />
     </FullscreenContainer>
   );
 };
