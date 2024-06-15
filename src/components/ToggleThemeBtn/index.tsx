1430672978c521ab680f0b3984025042718e18ed
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-15 12:32:44 +0300

    feat: save theme selection to async storage

--- src/components/ToggleThemeBtn/index.tsx
+++ src/components/ToggleThemeBtn/index.tsx
@@ -1,25 +1,32 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 
 import {ToggleButton} from '@/components/UI';
 import {ThemeVariant} from '@/constants/theme';
-import {useAppDispatch} from '@/store/hooks';
+import {useAppDispatch, useAppSelector} from '@/store/hooks';
 import {selectTheme} from '@/store/slices/themeSlice';
+import {selectedThemeSelector} from '@/store/slices/themeSlice/selectors';
 
 export const ToggleThemeBtn = () => {
   const {t} = useTranslation('profile');
   const dispatch = useAppDispatch();
+  const isDarkTheme =
+    useAppSelector(selectedThemeSelector) === ThemeVariant.DARK;
 
-  const onLeftSidePress = () => {
+  const onLeftSidePress = async () => {
+    AsyncStorage.setItem('theme', ThemeVariant.LIGHT);
     dispatch(selectTheme(ThemeVariant.LIGHT));
   };
 
-  const onRightSidePress = () => {
+  const onRightSidePress = async () => {
+    AsyncStorage.setItem('theme', ThemeVariant.DARK);
     dispatch(selectTheme(ThemeVariant.DARK));
   };
 
   return (
     <ToggleButton
+      initialSide={isDarkTheme ? 'right' : 'left'}
       left={{
         label: t('LightTheme'),
         onPress: onLeftSidePress,
