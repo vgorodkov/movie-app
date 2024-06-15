cf8de1ccb84b813b949252dcf9b63747f60cc39b
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-15 12:33:10 +0300

    feat: create AppProvier to init booked tickets data and provide theme

---
+++ src/components/AppProvider/index.tsx
@@ -0,0 +1,28 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React, {ReactNode} from 'react';
+import {StatusBar} from 'react-native';
+import {ThemeProvider} from 'styled-components';
+
+import {ThemeVariant} from '@/constants/theme';
+import {useBookedTicketsInit} from '@/hooks/useBookedTicketsInit';
+import {useAppSelector} from '@/store/hooks';
+import {selectedThemeSelector} from '@/store/slices/themeSlice/selectors';
+import {darkTheme} from '@/theme/darkTheme';
+import {lightTheme} from '@/theme/lightTheme';
+
+export const AppProvider = ({children}: {children: ReactNode}) => {
+  useBookedTicketsInit();
+  const isDark = useAppSelector(selectedThemeSelector) === ThemeVariant.DARK;
+  const theme = isDark ? darkTheme : lightTheme;
+  return (
+    <NavigationContainer theme={theme}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle={theme.dark ? 'light-content' : 'dark-content'}
+          backgroundColor={theme.colors.background}
+        />
+        {children}
+      </ThemeProvider>
+    </NavigationContainer>
+  );
+};
