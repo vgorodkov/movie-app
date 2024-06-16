54ddfb1389c92cdc82cb2020d7ef9d8a8c9b6ed2
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-16 20:14:48 +0300

    chore: change labels for localization keys

--- src/components/AuthVariants/constants.ts
+++ src/components/AuthVariants/constants.ts
@@ -3,28 +3,28 @@ import {AuthVariant} from './types';
 export const AUTH_VARIANTS: AuthVariant[] = [
   {
     name: 'account',
-    label: 'Create an Account',
+    label: 'AccountAuth',
     icon: require('@/assets/icons/person.png'),
     backgroundColor: '#404040',
     color: 'white',
   },
   {
     name: 'google',
-    label: 'Continue with Google',
+    label: 'GoogleAuth',
     icon: require('@/assets/icons/google.png'),
     backgroundColor: '#D4D4D4',
     color: '#00000085',
   },
   {
     name: 'facebook',
-    label: 'Sign up with Facebook',
+    label: 'FacebookAuth',
     icon: require('@/assets/icons/facebook.png'),
     backgroundColor: '#1877F2',
     color: 'white',
   },
   {
     name: 'github',
-    label: 'Sign up with GitHub',
+    label: 'GithubAuth',
     icon: require('@/assets/icons/github.png'),
     backgroundColor: '#000000',
     color: 'white',
