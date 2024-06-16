1e45701b3bd52950ba14c4cc0982f4cb724ad42a
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-16 17:49:32 +0300

    chore: create firebase consts

--- src/hooks/useMovieComments.ts
+++ src/hooks/useMovieComments.ts
@@ -1,19 +1,25 @@
 import firestore from '@react-native-firebase/firestore';
 import {useEffect, useState} from 'react';
 
-import {Comment} from '@/components/MovieComments/types';
+import {Comment} from '@/components/Movie/MovieComments/types';
+import {COMMENTS_COLLECTION} from '@/constants/firebase';
+import {useAppSelector} from '@/store/hooks';
+import {userSelector} from '@/store/slices/user/selectors';
 
 export const useMovieComments = (movieId: string) => {
+  const user = useAppSelector(userSelector);
   const [comments, setComments] = useState<Comment[]>([]);
   const [error, setError] = useState<null | string>(null);
 
+  const author = `${user?.name} ${user?.surname}`;
+
   const addComment = (newComment: Comment) => {
-    firestore().collection('comments').add(newComment);
+    firestore().collection(COMMENTS_COLLECTION).add(newComment);
   };
 
   useEffect(() => {
     const subscriber = firestore()
-      .collection('comments')
+      .collection(COMMENTS_COLLECTION)
       .orderBy('createdAt')
       .where('movieId', '==', movieId)
       .onSnapshot(
@@ -35,5 +41,5 @@ export const useMovieComments = (movieId: string) => {
     return () => subscriber();
   }, [movieId]);
 
-  return {comments, addComment, error};
+  return {comments, addComment, error, author};
 };
