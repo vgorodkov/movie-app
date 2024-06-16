5249924af53a670babf82d9ab8f92b27f426157c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-16 20:05:05 +0300

    fix: remove redundant try catch

--- src/utils/firebase/reauth.ts
+++ src/utils/firebase/reauth.ts
@@ -1,25 +1,17 @@
 import {firebase, FirebaseAuthTypes} from '@react-native-firebase/auth';
 
-import {showToast} from '../showToast';
-
 export const reauth = async (
   user: FirebaseAuthTypes.User,
   currentPassword: string,
 ) => {
-  try {
-    const providers = user.providerData.map(provider => provider.providerId);
-    if (providers.includes(firebase.auth.EmailAuthProvider.PROVIDER_ID)) {
-      const credentials = firebase.auth.EmailAuthProvider.credential(
-        user.email || '',
-        currentPassword,
-      );
-      await user.reauthenticateWithCredential(credentials);
-    } else if (
-      providers.includes(firebase.auth.GoogleAuthProvider.PROVIDER_ID)
-    ) {
-      await user.reauthenticateWithProvider(firebase.auth.GoogleAuthProvider);
-    }
-  } catch {
-    showToast('Problem with reauth');
+  const providers = user.providerData.map(provider => provider.providerId);
+  if (providers.includes(firebase.auth.EmailAuthProvider.PROVIDER_ID)) {
+    const credentials = firebase.auth.EmailAuthProvider.credential(
+      user.email || '',
+      currentPassword,
+    );
+    await user.reauthenticateWithCredential(credentials);
+  } else if (providers.includes(firebase.auth.GoogleAuthProvider.PROVIDER_ID)) {
+    await user.reauthenticateWithProvider(firebase.auth.GoogleAuthProvider);
   }
 };
