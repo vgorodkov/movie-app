6c6641ab6870a28b25ed9a7f93914d155a65f2cc
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-16 12:48:11 +0300

    chore: change time format of mock data

--- src/data/mockMovieSessions.ts
+++ src/data/mockMovieSessions.ts
@@ -1,14 +1,14 @@
-import {MovieSession} from '@/types/movies';
+import {MovieSession} from '@/types/booking';
 import {getRandomNumbersInRange} from '@/utils/getRandomNumbersInRange';
 
 export const movieSessions: MovieSession[] = [
   {
-    time: '2:30 PM - 4:25 PM',
+    time: '14:30 - 16:25',
     cinemaType: '1D',
     reservedSeats: getRandomNumbersInRange(0, 68, 25),
   },
   {
-    time: '7:30 PM - 9:25 PM',
+    time: '18:30 - 20:25',
     cinemaType: '3D',
     reservedSeats: getRandomNumbersInRange(0, 68, 45),
   },
