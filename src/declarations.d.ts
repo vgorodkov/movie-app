dc2901ff7e037728c6774e5f6b7ff7989d82970c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-16 17:42:29 +0300

    fix: declare svg module in assets folder to remove error

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -5,10 +5,3 @@ declare global {
     interface RootParamList extends RootStackParamList {}
   }
 }
-
-declare module '*.svg' {
-  import React from 'react';
-  import {SvgProps} from 'react-native-svg';
-  const content: React.FC<SvgProps>;
-  export default content;
-}
