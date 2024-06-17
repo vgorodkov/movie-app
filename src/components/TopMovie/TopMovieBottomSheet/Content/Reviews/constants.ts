552e111f0acf581b05bf2f6520a0a98d2e5b6278
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-17 12:27:30 +0300

    chore: use consts instead of hardcoded value

--- src/components/TopMovie/TopMovieBottomSheet/Content/Reviews/constants.ts
+++ src/components/TopMovie/TopMovieBottomSheet/Content/Reviews/constants.ts
@@ -1,5 +1,9 @@
 import {Dimensions} from 'react-native';
 
-const {width} = Dimensions.get('window');
+import {spacing} from '@/constants/spacing';
 
-export const REVIEW_WIDTH = width - 104;
+const {width} = Dimensions.get('window');
+const ARROW_WIDTH = 20;
+
+export const REVIEW_WIDTH =
+  width - spacing.sm * 2 - spacing.l - ARROW_WIDTH * 2 - spacing.s * 2;
