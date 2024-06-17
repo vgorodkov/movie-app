f77ef2452970985ba712df366665494f65eefcf6
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-17 17:03:14 +0300

    refactor: use constants

--- src/utils/getPasswordDifficulty.ts
+++ src/utils/getPasswordDifficulty.ts
@@ -1,20 +1,27 @@
+const LOW_LENGTH_BOUND = 4;
+const MEDIUM_LENGTH_BOUND = 8;
+const HIGHT_LENGTH_BOUD = 12;
+
+const HAS_CAPITAL_LETTER_REGEXP = /[A-Z]/;
+const HAS_SPECIAL_CHAR_REGEXP = /[.,!?/]/;
+
 export const getPasswordDifficulty = (password: string) => {
   let passwordDifficulty = 'Low';
   if (!password) {
     return '';
   }
   const length = password.length;
-  const hasUpperCase = /[A-Z]/.test(password);
-  const hasSpecialChar = /[.,!?/]/.test(password);
+  const hasUpperCase = HAS_CAPITAL_LETTER_REGEXP.test(password);
+  const hasSpecialChar = HAS_SPECIAL_CHAR_REGEXP.test(password);
 
-  if (length >= 4 && length <= 8) {
+  if (length >= LOW_LENGTH_BOUND && length <= MEDIUM_LENGTH_BOUND) {
     passwordDifficulty = 'Low';
   } else if (
-    (length > 8 && hasUpperCase && !hasSpecialChar) ||
+    (length > MEDIUM_LENGTH_BOUND && hasUpperCase && !hasSpecialChar) ||
     (!hasUpperCase && hasSpecialChar)
   ) {
     passwordDifficulty = 'Medium';
-  } else if (length > 12 && hasUpperCase && hasSpecialChar) {
+  } else if (length > HIGHT_LENGTH_BOUD && hasUpperCase && hasSpecialChar) {
     passwordDifficulty = 'High';
   }
   return passwordDifficulty;
