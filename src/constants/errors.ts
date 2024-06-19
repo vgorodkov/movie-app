36b990c2fdd331e3b8c143db87ce4f9450d8d620
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-19 14:19:08 +0300

    chore: create common errors mapper object

---
+++ src/constants/errors.ts
@@ -0,0 +1,7 @@
+export const errorMessages: {[key: string]: string} = {
+  'auth/email-already-in-use': 'That email address is already in use!',
+  'auth/invalid-email': 'That email address is invalid!',
+  'auth/invalid-credential': 'Auth credential is invalid!',
+  'auth/account-exists-with-different-credential':
+    'An account already exists with the same email address but different sign-in credentials',
+};
