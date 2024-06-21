3595450f8f53e47065c057c8f66997df8e1b1a31
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-21 15:06:03 +0300

    feat: setup detox

--- metro.config.js
+++ metro.config.js
@@ -7,13 +7,20 @@ const {assetExts, sourceExts} = defaultConfig.resolver;
  *
  * @type {import('metro-config').MetroConfig}
  */
+
+const defaultSourceExts =
+  require('metro-config/src/defaults/defaults').sourceExts;
+
 const config = {
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
   },
   resolver: {
     assetExts: assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'svg'],
+    sourceExts:
+      process.env.MY_APP_MODE === 'mocked'
+        ? ['mock.ts', ...defaultSourceExts, 'svg']
+        : [...defaultSourceExts, 'svg'],
   },
 };
 
