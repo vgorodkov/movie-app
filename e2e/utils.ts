1ad717db2a3ec24f56ec755a1fd7b2cdd1534c72
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-22 15:18:11 +0300

    tests: add utils for screenshot testing

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,2 +1,45 @@
+import {execSync} from 'child_process';
+import {readFileSync} from 'fs';
+
 export const byId = (id: string) => element(by.id(id));
 export const byText = (text: string) => element(by.text(text));
+
+export const setDemoMode = async () => {
+  if (device.getPlatform() === 'ios') {
+    execSync(
+      'xcrun simctl status_bar "iPhone 11" override --time "12:00" --batteryState charged --batteryLevel 100 --wifiBars 3 --cellularMode active --cellularBars 4',
+    );
+  } else {
+    // enter demo mode
+    execSync('adb shell settings put global sysui_demo_allowed 1');
+    // display time 12:00
+    execSync(
+      'adb shell am broadcast -a com.android.systemui.demo -e command clock -e hhmm 1200',
+    );
+    // Display full mobile data with 4g type and no wifi
+    execSync(
+      'adb shell am broadcast -a com.android.systemui.demo -e command network -e mobile show -e level 4 -e datatype 4g -e wifi false',
+    );
+    // Hide notifications
+    execSync(
+      'adb shell am broadcast -a com.android.systemui.demo -e command notifications -e visible false',
+    );
+    // Show full battery but not in charging state
+    execSync(
+      'adb shell am broadcast -a com.android.systemui.demo -e command battery -e plugged false -e level 100',
+    );
+  }
+};
+
+export const expectBitmapsToBeEqual = (
+  imagePath: string,
+  expectedImagePath: string,
+) => {
+  const bitmapBuffer = readFileSync(imagePath);
+  const expectedBitmapBuffer = readFileSync(expectedImagePath);
+  if (!bitmapBuffer.equals(expectedBitmapBuffer)) {
+    throw new Error(
+      `Expected image at ${imagePath} to be equal to image at ${expectedImagePath}, but it was different!`,
+    );
+  }
+};
