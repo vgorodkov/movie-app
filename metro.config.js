a084f4900ec927db5299ec78058e44e04abc1778
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-06-22 15:15:09 +0300

    chore: remove redundant import

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
-const {assetExts, sourceExts} = defaultConfig.resolver;
+const {assetExts} = defaultConfig.resolver;
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
